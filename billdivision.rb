ca277f599ef24e1e9933769773bcd5a6a5af8975
dylansarikas <dylan.sarikas@gmail.com>
2023-11-08 15:37:44 -0600

    adds next challenge

--- billdivision.rb
+++ billdivision.rb
@@ -1,10 +1,19 @@
 def bonAppetit(bill, k, b)
-    for thing in bill
-        while x > code
+    #p bill
+    bill.delete_at(k)
+    #p bill
+    output = 0
+    total = bill.sum
+    if (total/2) == b
+        output = "Bon Appetit"
+    else
+        output = (b - (total/2))
+    end
 
-        end
-    # Write your code here
+    puts output
+    return output
 
 end
 
-bonAppetit([[4, 1], [3, 10, 2, 9], [7]])
+bonAppetit([3, 10, 2, 9], 1, 7)
+bonAppetit([3, 10, 2, 9], 1, 12)
