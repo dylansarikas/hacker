8f16d4759d1a9111484580b777c35196096244f8
dylansarikas <dylan.sarikas@gmail.com>
2024-08-29 17:08:31 -0500

    removes comments

--- graderounding.rb
+++ graderounding.rb
@@ -1,7 +1,4 @@
 def gradingStudents(grades)
-    #0 1 2 5 6 7 No Round
-    #3 4 8 9 Round
-    #% > 2 Round
     grades.each_with_index do |grade, index|
         if (grade%5) > 2  && (grade>37)
             grades[index] = grade + (5-(grade%5))
@@ -11,14 +8,6 @@ def gradingStudents(grades)
     p grades
 
 end
-
-# puts(0%5) 0
-# puts(2%5) 2
-# puts(3%5) 3
-# puts(5%5) 5
-# puts(7%5) 2
-# puts(8%5) 3
-# puts(9%5) 4
 
 abc = [73, 67, 38, 33]
 gradingStudents(abc)
