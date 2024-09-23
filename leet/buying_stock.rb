5d5fa54f1b7eeb3701d5c28c1eeff9e828c6f024
dylansarikas <dylan.sarikas@gmail.com>
2024-09-23 13:01:18 -0500

    adds todays leetcode

---
+++ leet/buying_stock.rb
@@ -0,0 +1,22 @@
+def max_profit(prices)
+    profit = 0
+    buy = 0
+    sell = 1
+    while sell < prices.length
+        # puts "wow"
+        # puts prices[buy]
+        # puts prices[sell]
+        if prices[sell] < prices[buy]
+            buy = sell
+            sell = buy + 1
+        else
+            profit = [profit, (prices[sell] - prices[buy])].max
+        end
+        sell += 1
+    end
+
+    puts profit
+end
+
+max_profit([7,1,5,3,6,4])
+max_profit([7,6,4,3,1])
