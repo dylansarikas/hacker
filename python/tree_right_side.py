738f0d7a9266ec0fbb1e147355412989fce813cb
dylansarikas <dylan.sarikas@gmail.com>
2024-10-14 17:10:50 -0500

    adds todays neetcode

---
+++ python/tree_right_side.py
@@ -0,0 +1,20 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            length = len(q)
+            level = []
+            for i in range(length):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            
+            if level:
+                result.append(level[-1])
+            
+        return result
