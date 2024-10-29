9f5ed19b0773325eceb2a3a76636e44506cfc10f
dylansarikas <dylan.sarikas@gmail.com>
2024-10-29 15:08:02 -0500

    adds todays leetcode

--- python/two_sum.py
+++ python/two_sum.py
@@ -1,1 +1,12 @@
-print("good stuff")
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        numIndices = {}
+
+        for i, n in enumerate(nums):
+            difference = target - n
+            if difference in numIndices:
+                return [numIndices[difference], i]
+            
+            numIndices[n] = i
+
+        return
